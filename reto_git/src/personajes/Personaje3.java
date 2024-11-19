0479947d71cc94be49493d601a3d3a7365b1620d
Lucca <luccasolerramos@gmail.com>
2024-11-19 20:41:58 +0100

    Lucca añade personaje3

---
+++ reto_git/src/personajes/Personaje3.java
@@ -0,0 +1,14 @@
+package personajes;
+
+public class Personaje3 {
+
+	public Personaje3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
