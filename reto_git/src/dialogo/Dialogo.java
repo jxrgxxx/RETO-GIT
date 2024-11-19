c7ab09ece0088bb1e8137795cf1f576fcfc0151a
jxrgxx <24.damt.jleileon@fundacionmontessori.com>
2024-11-19 20:02:19 +0100

    primer commit, añado el proyecto de java

---
+++ reto_git/src/dialogo/Dialogo.java
@@ -0,0 +1,10 @@
+package dialogo;
+
+public class Dialogo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ reto_git/src/personajes/Personaje1.java
@@ -0,0 +1,10 @@
+package personajes;
+
+public class Personaje1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
