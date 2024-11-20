ed0f174c5dd175faffdbe7e21c0b5d3521cb3ba6
Victor Cebollada <24.damt.victorcebolladafundacionmontes.com>
2024-11-20 17:14:19 +0100

    Victor actualiza personaje4

--- reto_git/src/personajes/Personaje4.java
+++ reto_git/src/personajes/Personaje4.java
@@ -4,5 +4,21 @@ public class Personaje4 {
 
 	public static void main(String[] args) {
 		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
+		System.out.print("Personaje4");
 	}
 }
